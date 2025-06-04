ed62e454457e81765c53dbdde34008979490af7c
user <sooooooobinn@gmail.com>
2025-06-04 14:14:07 +0900

    첫번쩨 커밋

---
+++ src/main/java/com/example/board/entity/Board.java
@@ -0,0 +1,7 @@
+package com.example.board.entity;
+
+public class Board {
+    private Long id;
+    private String title;
+    private String content;
+}
---
+++ src/main/java/com/example/board/repository/BoardRepository.java
@@ -0,0 +1,11 @@
+package com.example.board.repository;
+
+import com.example.board.entity.Board;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardRepository {
+    private List<Board> boards = new ArrayList<>();
+
+}
